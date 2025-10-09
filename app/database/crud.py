e49c4e2db06672ffb6ca9702e20498ff88de95d4
frez <rezende.f@outlook.com>
2025-10-09 09:21:38 +0800

    save_organic_results() updated to include the two helper functions

--- app/database/crud.py
+++ app/database/crud.py
@@ -14,16 +14,25 @@ async def get_or_create_location(session, location_name: str) -> Location:
 
         Arg: session, location name
 
-        Usage: Returns either an existing or new location object 
-    """
-    statement = select(Location).where(Location.location == location_name)
-    result = await session.exec(statement)
-    location = result.first()
+        Usage: called from within save_organic_results() methof
 
-    if location is None:
-        location = Location(location=location_name)
-        session.add(location)
-        await session.flush()  # Get the ID without committing.
+        Output: returns an existing or new location object
+    """
+    try:
+        statement = select(Location).where(Location.location == location_name)
+        result = await session.exec(statement)
+        location = result.first()
+
+        if location is None:
+            location = Location(location=location_name)
+            session.add(location)
+            await session.flush()  # Get the ID without committing.
+    
+    except Exception as e:
+        # NOTE: Set up exception class once error output is clear
+        print(f"Error: {e}")
+        print(type(e))
+        print(repr(e))
 
     return location
 
@@ -38,65 +47,86 @@ async def get_or_create_keyword(
         Helper function: Get existing keyword or
         create new one.
 
-        Usage: 
+        Usage: called from within save_organic_results() method
+
+        Output: Existing or new keyword object
     """
-    statement = select(Keyword).where(
-        Keyword.keywords == keyword_text,
-        Keyword.location_id
-    )
-    result = await session.exec(statement)
-    keyword = result.first()
-
-    if keyword is None:
-        keyword = Keyword(
-            keywords=keyword_text,
-            created_date=created_date,
-            location=location,
+    try:
+        statement = select(Keyword).where(
+            Keyword.keywords == keyword_text,
+            Keyword.location_id
         )
-        session.add(keyword)
-        await session.flush()
-    
-    return keyword
+        result = await session.exec(statement)
+        keyword = result.first()
 
+        if keyword is None:
+            keyword = Keyword(
+                keywords=keyword_text,
+                created_date=created_date,
+                location=location,
+            )
+            session.add(keyword)
+            await session.flush()
+
+    except Exception as e:
+        # NOTE: Set up exception class once error output is clear
+        print(f"Error: {e}")
+        print(type(e))
+        print(repr(e))
+
+    return keyword
 
 
 # https://sqlmodel.tiangolo.com/tutorial/relationship-attributes/create-and-update-relationships/#create-instances-with-relationship-attributes
 async def save_organic_results(data: list[dict]):
     """
-     Save organic search results to database.
+    Save organic search results to database.
+
     Args:
         data: List of search result dictionaries from SERP API
+
     Usage:
         await save_organic_results(api_response_data)
+
+        This function will call two helper functions:
+
+        get_or_create_location() -> Location
+
+            - Checks for existing location,
+            if None create location.
+
+        get_or_create_keyword() -> Keyword
+
+            - Checks for existing keyword,
+            if None create keyword:
     """
     async with async_session() as session:
-        # NOTE: logic below must be implemented
-        # to check if location exists
-        # if so then keywords must be added to existing location
 
         location_name = data[0]['location']
-        location = await get_or_create_location(location_name)
-        
-            
+        location = await get_or_create_location(session, location_name)
 
-            for item in data:
-                keyword_text = item['keyword']
-                keyword_date = item['created_date']
-
-                keyword_query = get_keyword(key_word_text)
-                # List comprehension to add ranks at once
-                # As opposed to adding a rank after each iteration on traditional loop
-                rank = [
-                    OrganicRank(
+        for item in data:
+            keyword_text = item['keyword']
+            created_date = item['created_date']
+            keyword = await get_or_create_keyword(
+                    session,
+                    keyword_text,
+                    created_date,
+                    location
+                )
+            # List comprehension to add ranks at once
+            # As opposed to adding a rank after each iteration on traditional loop
+            rank = [
+                OrganicRank(
                         title=rank_item['title'],
                         source=rank_item['source'],
                         position=rank_item['position'],
                         link=rank_item['link'],
-                        keyword=search_term,  # Set relationship between OrganicRank and Keyword model.
-                    )
-                    for rank_item in item['rank']
-                ]
-                session.add_all(rank)  # Add all ranks at once 
+                        keyword=keyword,  # Set relationship between OrganicRank and Keyword model.
+                )
+                for rank_item in item['rank']
+            ]
+            session.add_all(rank)  # Add all ranks at once    
         await session.commit()
 
 
@@ -131,8 +161,8 @@ def main():
     Enables running/testing functions
     as a module
     """
-    asyncio.run(retrieve_target_url())
-
+    #asyncio.run(retrieve_target_url())
+    asyncio.run(get_or_create_location(session, ))
 
 if __name__ == "__main__":
     main()
