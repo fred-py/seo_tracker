809881c013d66cc491a4759bb0066defe2ce5f86
frez <rezende.f@outlook.com>
2025-11-26 17:34:53 +0800

    offset accomplished using scattermode and scattergap and a few mode updates to the p[lot layout

--- app/services/seo_analysis_plotly_graph.py
+++ app/services/seo_analysis_plotly_graph.py
@@ -23,6 +23,13 @@ data_home = asyncio.run(get_url_rank_by_service_location(
 # Sorting by date stops the lines jumping backwards on the chart.
 df = pl.DataFrame(data_home).sort(by='date')
 
+# NOTE: When scattermode is set to 'group' it will override
+# autorange and display 0 as min. value on yaxis 
+# To fix this, assign min and max rank position from df
+# and explicitly to set yaxis range under update_layout
+rank_min = df['position'].min()
+rank_max = df['position'].max()
+
 fig = go.Figure()
 
 for keyword in df['keyword'].unique():  # Manually loop over unique keyword column - plotly express does this automatically
@@ -36,6 +43,39 @@ for keyword in df['keyword'].unique():  # Manually loop over unique keyword colu
         name=keyword,
     ))
 
-fig.update_yaxes(autorange='reversed')  # set Y axis to descending order
+fig.update_yaxes(
+    #autorange='reversed',   # set Y axis to descending order
+    tickformat='d',         # Format as integers (no decimals)
+    dtick=1,                # Show tick every 1 unit (optional but ensures integer spacing)
+
+    )
+
+fig.update_layout(
+    title=dict(
+        text='SEO Performance - Google Search Position Tracking',
+        # xref defaults to container. Spans the entire width of the plot
+        # paper refers to the width of the plotting area only
+        # Setting to paper moves the title slightly to the right
+        xref='paper',
+    ),
+    xaxis=dict(
+        title=dict(
+            text='Date'
+        )
+    ),
+    yaxis=dict(
+        # To reverse yaxis range, rank_max is passed as the first argument
+        # Adding/subtracting value to min and max range stops chart from
+        # cutting off markers on both end eg. rank_max + 0.2
+        range=[rank_max + 0.2, rank_min - 0.2],
+        title=dict(
+            text='Rank position'
+        ),
+    ),
+    # Offset lines to be visible when overlap occurs
+    scattermode='group',
+    scattergap=0.5,  
+)
+
 
 fig.show()
