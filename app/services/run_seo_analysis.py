2adb58033979dcafef3d57c657216d316d4daf77
frez <rezende.f@outlook.com>
2026-01-15 19:35:32 +0800

    commit before merge - new update_fig_layout method tested and running succefully

--- app/services/run_seo_analysis.py
+++ app/services/run_seo_analysis.py
@@ -126,6 +126,91 @@ def plot_lines_markers_ranked(fig, df, unranked_df, df_new) -> go.Figure:
     return fig
 
 
+def update_fig_layout(fig: go.Figure) -> go.Figure:
+    """
+    This function carries out layout updates of
+    plotly figure object
+    
+    Args: go.Figure
+    
+    Output: updated go.Figure object
+    """
+
+    try:
+        fig.update_yaxes(
+        #autorange='reversed',   # set Y axis to descending order
+        tickformat='d',         # Format as integers (no decimals)
+        dtick=1,                # Show tick every 1 unit (optional but ensures integer spacing)
+
+        )
+
+        fig.update_layout(
+            title=dict(
+                text='SEO Performance - Google Search Position Tracking',
+                # xref defaults to container. Spans the entire width of the plot
+                # paper refers to the width of the plotting area only
+                # Setting to paper moves the title slightly to the right
+                xref='paper',
+                subtitle=dict(
+                    text=f'Location: {location}  |  Service: {service}  |  URL: {url}'
+                ),
+            ),
+
+            #legend_title_text='Ranked Keywords',
+
+            legend=dict(
+                title=dict(
+                    text='Ranked Keywords',
+                ),
+                y=0.99,
+                x=1.02,
+                xanchor='left',
+            ),
+            legend2=dict(
+                title=dict(
+                    text='Unranked Keywords',
+                    
+                ),
+                y=0.5,
+                x=1.02,
+                xanchor='left',
+            ),
+            legend3=dict(
+                title=dict(
+                    text='Recently Ranked Keywords',
+                    
+                ),
+                y=0.1,
+                x=1.02,
+                xanchor='left',
+            ),
+
+            xaxis=dict(
+                title=dict(
+                    text='Date'
+                )
+            ),
+
+            yaxis=dict(
+                # To reverse yaxis range, rank_max is passed as the first argument
+                # Adding/subtracting value to min and max range stops chart from
+                # cutting off markers on both end eg. rank_max + 0.2
+                range=[rank_max + 0.2, rank_min - 0.2],
+                title=dict(
+                    text='Rank Position (within top 10 organic results)'
+                ),
+            ),
+
+            # Offset lines to be visible when overlap occurs
+            scattermode='group',
+            scattergap=0.1,
+        )
+    except Exception as e:
+        raise e
+
+    return fig
+
+
 # NOTE: When scattermode is set to 'group' it will override
 # autorange and display 0 as min. value on yaxis 
 # To fix this, assign min and max rank position from df
@@ -139,75 +224,8 @@ service = df['service'][0]
 
 fig = go.Figure()
 
-fig = plot_lines_markers_ranked(fig, df, unranked_df, df_new)
+fig_data = plot_lines_markers_ranked(fig, df, unranked_df, df_new)
 
-fig.update_yaxes(
-    #autorange='reversed',   # set Y axis to descending order
-    tickformat='d',         # Format as integers (no decimals)
-    dtick=1,                # Show tick every 1 unit (optional but ensures integer spacing)
+fig_layout = update_fig_layout(fig_data)
 
-    )
-
-fig.update_layout(
-    title=dict(
-        text='SEO Performance - Google Search Position Tracking',
-        # xref defaults to container. Spans the entire width of the plot
-        # paper refers to the width of the plotting area only
-        # Setting to paper moves the title slightly to the right
-        xref='paper',
-        subtitle=dict(
-            text=f'Location: {location}  |  Service: {service}  |  URL: {url}'
-        ),
-    ),
-
-    #legend_title_text='Ranked Keywords',
-
-    legend=dict(
-        title=dict(
-            text='Ranked Keywords',
-        ),
-        y=0.99,
-        x=1.02,
-        xanchor='left',
-    ),
-    legend2=dict(
-        title=dict(
-            text='Unranked Keywords',
-            
-        ),
-        y=0.5,
-        x=1.02,
-        xanchor='left',
-    ),
-    legend3=dict(
-        title=dict(
-            text='Recently Ranked Keywords',
-            
-        ),
-        y=0.1,
-        x=1.02,
-        xanchor='left',
-    ),
-
-    xaxis=dict(
-        title=dict(
-            text='Date'
-        )
-    ),
-
-    yaxis=dict(
-        # To reverse yaxis range, rank_max is passed as the first argument
-        # Adding/subtracting value to min and max range stops chart from
-        # cutting off markers on both end eg. rank_max + 0.2
-        range=[rank_max + 0.2, rank_min - 0.2],
-        title=dict(
-            text='Rank Position (within top 10 organic results)'
-        ),
-    ),
-
-    # Offset lines to be visible when overlap occurs
-    scattermode='group',
-    scattergap=0.1,  
-)
-
-fig.show()
+fig_layout.show()
